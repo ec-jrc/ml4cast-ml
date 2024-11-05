13b09cfbe076ac6b67437c27ef50942fbf4f6c99
meronmi <michele.meroni@gmail.com>
2024-11-05 14:19:34 +0100

    condor split in two commit

--- tuner.py
+++ tuner.py
@@ -90,7 +90,156 @@ def tuneA(run_name, config_fn, tune_on_condor, runType):
     spec_files_list = glob.glob(os.path.join(config.models_spec_dir, '*.json'))
     return spec_files_list
 
+def checkExistingSubmit(condor_task_list_base_name, condor_task_list_fn, config, spec_files_list):
+    # If the file already exists it means that we are re-running Condor for some reasons (get stopped somehow)
+    # In this case copy the HT_condor_task_arguments to HT_condor_task_arguments_all and keep
+    # only the entries that were not successful (sya that logs will be overwritten and must be moved a folder,
+    # wait for Y from keyboard)
+
+    print(condor_task_list_fn)
+    print('HT_condor_task_arguments.txt already exists')
+    print('>Jobs that produced already outputs will be skipped')
+    print('Log files will be deleted, move to a named dir if you want to keep them')
+    pro = input('Type Y to proceed\n')
+    if pro == 'Y':
+        fn_rename = condor_task_list_base_name.split('.')[0] + '_all.txt'
+        os.rename(condor_task_list_fn, os.path.join(config.models_dir, fn_rename))
+        # update spec_files_list
+        new_file_list = []
+        for el in spec_files_list:
+            # make the expected output name
+            with open(el, 'r') as fp:
+                uset = json.load(fp)
+            myID = uset['runID']
+            myID = f'{myID:06d}'
+            fn_to_check = os.path.join(config.models_out_dir, 'ID_' + str(myID) +
+                                       '_crop_' + uset['crop'] + '_Yield_' + uset['algorithm'] + '_output.csv')
+            if not os.path.isfile(fn_to_check):
+                new_file_list.append(el)
+        spec_files_list = new_file_list
+        print('List of files with no output:')
+        scount = 0
+        for s in spec_files_list:
+            print(str(scount), s)
+            scount = scount + 1
+        return spec_files_list
+    else:
+        sys.exit('Tuner terminated by user')
 def tuneB(run_name, config_fn, tune_on_condor, runType, spec_files_list):
+    # ----------------------------------------------------------------------------------------------------------
+    # new version that splits big lists of task > 7500 in two blocks, 5000 and the rest
+    # PART B
+    nMaxTask = 7500
+    start_time = time.time()
+    config = a10_config.read(config_fn, run_name, run_type=runType)
+
+    if tune_on_condor == False:
+        # get the produced spec file list
+        for fn in spec_files_list:
+            print(fn)
+            d090_model_wrapper.fit_and_validate_single_model(fn, config, runType)
+        #F100_analyze_hindcast_output.gather_output(config.models_out_dir)
+        print("--- %s seconds ---" % (time.time() - start_time))
+    else:
+        # running with condor
+        dir_condor_submit = config.models_dir
+        # make the task list (id, filename full path)
+        condor_task_list_base_name = 'HT_condor_task_arguments.txt'
+        condor_task_list_fn = os.path.join(config.models_dir, condor_task_list_base_name)
+        spec_files_list1 = spec_files_list
+        if len(spec_files_list) > nMaxTask:
+            spec_files_list1 = spec_files_list[0:7500]
+            spec_files_list2 = spec_files_list[7500:-1]
+            condor_task_list_base_name2 = 'HT_condor_task_arguments2.txt'
+            condor_task_list_fn2 = os.path.join(config.models_dir, condor_task_list_base_name2)
+        # If the file already exists it means that we are re-running Condor for some reasons (get stopped somehow)
+        # In this case copy the HT_condor_task_arguments to HT_condor_task_arguments_all and keep
+        # only the entries that were not successful (sya that logs will be overwritten and must be moved a folder,
+        # wait for Y from keyboard)
+        if os.path.isfile(condor_task_list_fn):
+            spec_files_list2 = checkExistingSubmit(condor_task_list_base_name, condor_task_list_fn, config, spec_files_list2)
+        if len(spec_files_list) > nMaxTask:
+            # there was a second ht condor
+            if os.path.isfile(condor_task_list_fn2):
+                spec_files_list1 = checkExistingSubmit(condor_task_list_base_name2, condor_task_list_fn2, config, spec_files_list)
+
+
+        if len(spec_files_list1) == 0 and len(spec_files_list2) == 0:
+            print('No files to re-run, execution will stop')
+            sys.exit()
+        # spec_files_list1 exists if I am here
+        if len(spec_files_list1) > 0:
+            if os.path.exists(condor_task_list_fn):
+                os.remove(condor_task_list_fn)
+            f_obj = open(condor_task_list_fn, 'a')
+            for el in spec_files_list1:
+                f_obj.write(f'{str(el)} {config_fn} {run_name} {runType}\n')
+            f_obj.close()
+            # Make sure that the run.sh in this project is executable (# chmod 755 run.sh)
+
+            # adjust the condor.submit template
+            condSubPath = os.path.join(dir_condor_submit, 'condor.submit')
+            with open('G_HTCondor/condor.submit_template') as tmpl:
+                content = tmpl.read()
+                content = content.format(AOI=config.AOI, root_dir=config.models_dir) #, shDestination=shDestination)
+            with open(condSubPath, 'w') as out:
+                out.write(content)
+            # Make the dirs for condor output on /mnt/jeoproc/log/ml4castproc/, clean content
+            Path(os.path.join('/mnt/jeoproc/log/ml4castproc', config.AOI, 'out')).mkdir(parents=True, exist_ok=True)
+            Path(os.path.join('/mnt/jeoproc/log/ml4castproc', config.AOI, 'err')).mkdir(parents=True, exist_ok=True)
+            Path(os.path.join('/mnt/jeoproc/log/ml4castproc', config.AOI, 'log')).mkdir(parents=True, exist_ok=True)
+            remove_files(os.path.join('/mnt/jeoproc/log/ml4castproc', config.AOI, 'out'))
+            remove_files(os.path.join('/mnt/jeoproc/log/ml4castproc', config.AOI, 'err'))
+            remove_files(os.path.join('/mnt/jeoproc/log/ml4castproc', config.AOI, 'log'))
+            # Launch condor (sudo -u ml4castproc condor_submit condor.submit)
+            run_cmd = ['sudo', '-u', 'ml4castproc', 'condor_submit', condSubPath]
+            p = subprocess.run(run_cmd, shell=False, input='\n', capture_output=True, text=True)
+            if p.returncode != 0:
+                print('ERR', p.stderr)
+                raise Exception('Step subprocess error')
+            print('Batch submitted by tuner: ' + 'ml4cast_' + config.AOI)
+            print(p.stdout)
+
+            if len(spec_files_list) > nMaxTask:
+                # the task was big there is another task to run, but we need to wait an hour
+                time.sleep(60 * 60)
+                if len(spec_files_list2) > 0:
+                    if os.path.exists(condor_task_list_fn2):
+                        os.remove(condor_task_list_fn2)
+                    f_obj = open(condor_task_list_fn2, 'a')
+                    for el in spec_files_list2:
+                        f_obj.write(f'{str(el)} {config_fn} {run_name} {runType}\n')
+                    f_obj.close()
+                    # Make sure that the run.sh in this project is executable (# chmod 755 run.sh)
+                    # adjust the condor.submit template
+                    condSubPath = os.path.join(dir_condor_submit, 'condor.submit2')
+                    with open('G_HTCondor/condor.submit_template') as tmpl:
+                        content = tmpl.read()
+                        content = content.format(AOI=config.AOI,
+                                                 root_dir=config.models_dir)  # , shDestination=shDestination)
+                    with open(condSubPath, 'w') as out:
+                        out.write(content)
+                    # Make the dirs for condor output on /mnt/jeoproc/log/ml4castproc/, clean content
+                    Path(os.path.join('/mnt/jeoproc/log/ml4castproc', config.AOI, 'out')).mkdir(parents=True,
+                                                                                                exist_ok=True)
+                    Path(os.path.join('/mnt/jeoproc/log/ml4castproc', config.AOI, 'err')).mkdir(parents=True,
+                                                                                                exist_ok=True)
+                    Path(os.path.join('/mnt/jeoproc/log/ml4castproc', config.AOI, 'log')).mkdir(parents=True,exist_ok=True)
+                    if len(spec_files_list1) == 0:
+                        remove_files(os.path.join('/mnt/jeoproc/log/ml4castproc', config.AOI, 'out'))
+                        remove_files(os.path.join('/mnt/jeoproc/log/ml4castproc', config.AOI, 'err'))
+                        remove_files(os.path.join('/mnt/jeoproc/log/ml4castproc', config.AOI, 'log'))
+                    # Launch condor (sudo -u ml4castproc condor_submit condor.submit)
+                    run_cmd = ['sudo', '-u', 'ml4castproc', 'condor_submit', condSubPath]
+                    p = subprocess.run(run_cmd, shell=False, input='\n', capture_output=True, text=True)
+                    if p.returncode != 0:
+                        print('ERR', p.stderr)
+                        raise Exception('Step subprocess error')
+                    print('Batch submitted by tuner: ' + 'ml4cast_' + config.AOI)
+                    print(p.stdout)
+
+
+def tuneBOLD(run_name, config_fn, tune_on_condor, runType, spec_files_list):
     # ----------------------------------------------------------------------------------------------------------
     # PART B
     start_time = time.time()
@@ -107,8 +256,8 @@ def tuneB(run_name, config_fn, tune_on_condor, runType, spec_files_list):
         # running with condor
         dir_condor_submit = config.models_dir
         # make the task list (id, filename full path)
-        fn_condor_task_list = 'HT_condor_task_arguments.txt'
-        condor_task_list_fn = os.path.join(config.models_dir, fn_condor_task_list)
+        condor_task_list_base_name = 'HT_condor_task_arguments.txt'
+        condor_task_list_fn = os.path.join(config.models_dir, condor_task_list_base_name)
         # If the file already exists it means that we are re-running Condor for some reasons (get stopped somehow)
         # In this case copy the HT_condor_task_arguments to HT_condor_task_arguments_all and keep
         # only the entries that were not successful (sya that logs will be overwritten and must be moved a folder,
@@ -119,7 +268,7 @@ def tuneB(run_name, config_fn, tune_on_condor, runType, spec_files_list):
             print('Log files will be deleted, move to a named dir if you want to keep them')
             pro = input('Type Y to proceed\n')
             if pro == 'Y':
-                fn_rename = fn_condor_task_list.split('.')[0] + '_all.txt'
+                fn_rename = condor_task_list_base_name.split('.')[0] + '_all.txt'
                 os.rename(condor_task_list_fn, os.path.join(config.models_dir, fn_rename))
                 # update spec_files_list
                 new_file_list = []
