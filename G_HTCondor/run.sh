6a26a82d2b1b7b46c92d31b480099929e8f753bd
meronmi <michele.meroni@gmail.com>
2024-11-18 13:42:29 +0100

    added export for matplotlib in run.sh

--- G_HTCondor/run.sh
+++ G_HTCondor/run.sh
@@ -4,7 +4,7 @@
 
 #echo "Launching condor on test"
 #echo ${3} ${4} ${5}
-# export MPLCONFIGDIR=/scratch/ml4castproc
+export MPLCONFIGDIR=/tmp
 # conda activate /mnt/jeoproc/envs/ML4CAST/YF3_dock_p311
 /mnt/jeoproc/envs/ML4CAST/YF3_dock_p311/bin/python3.11 /eos/jeodpp/data/projects/ML4CAST/ml4cast-ml/condor_launcher.py ${3} ${4} ${5} ${6}
 #/mnt/jeoproc/envs/BDA/test_env/condabin/conda run -p /mnt/jeoproc/envs/ML4CAST/YF3_dock_p311/ python3.11 /eos/jeodpp/data/projects/ML4CAST/ml4cast-ml/condor_launcher.py ${3} ${4} ${5}
